e53e590b74260098c53239ac52212d34c657a99a
Luchopst <poncestigaardluciano@gmail.com>
2025-11-26 17:48:37 -0300

    agrego email

--- parcial/settings.py
+++ parcial/settings.py
@@ -140,3 +140,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD") # no tu contraseña normal, sino un App Password
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
